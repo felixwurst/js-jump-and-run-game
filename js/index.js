33a7785be151ca3f5c65b20eb72b7be8ba4a7d7e
felix.wurst <felix.wurst@gmail.com>
2020-04-24 01:26:16 +0200

    added comments and levels

--- js/index.js
+++ js/index.js
@@ -28,17 +28,22 @@ var simpleLevelPlan = `
 
 class Level {
     constructor(plan) {
-        let rows = plan.trim().split("\n").map(l => [...l]);
+        let rows = plan.trim().split("\n").map(item => item.split("")) 
+        //let rows = plan.trim().split("\n").map(l => [...l]);
+        // console.log(rows);
+        
         this.height = rows.length;
         this.width = rows[0].length;
         // stored in an array of objects
         this.startActors = [];
+        
         // each 'rows' holds an array of arrays with characters, the rows of the level plan.
         // map passes the array index as a second argument to the mapping function,
         // which tells us the x- and y-coordinates of a given character.
         this.rows = rows.map((row, y) => {
             return row.map((ch, x) => {
                 let type = levelChars[ch];
+                // console.log(type);
                 if (typeof type == "string") {
                     return type;
                 }
@@ -51,7 +56,7 @@ class Level {
 
 /// --------------- Set Status -------------- ///
 
-//State class to track the state of a running game.
+// State class to track the state of a running game
 class State {
     constructor(level, actors, status) {
         this.level = level;
@@ -73,6 +78,8 @@ class State {
 // The position of the actor is stored as a Vec object.
 // the Vec class is used for the two-dimensional values, such as
 // the position and size of actors.
+// ? Vec object with x- and y-value --> Vec {x: 1, y: 1}
+// ? function plus for vector addition, function times for vector multiplication, e.g. for jumping
 class Vec {
     constructor(x, y) {
         this.x = x;
@@ -99,11 +106,15 @@ class Player {
         return "player";
     }
     static create(pos) {
-        return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
+        let newPosVec = new Vec(0, -0.5);
+        let speedVec = new Vec(0, 0);
+        let newPos = pos.plus(newPosVec);
+        return new Player(newPos, speedVec);
     }
 }
 // the size of neither/non of the Actors will change. So it is stored on .prototype rather than in the object itself
 // using type would create and return a new Vec object every time the property is read
+// ? using type?
 Player.prototype.size = new Vec(0.8, 1.5);
 
 class Lava {
@@ -118,19 +129,23 @@ class Lava {
     // conditions to check for 'moving' lava
     static create(pos, ch) {
         if (ch == "=") {
-            return new Lava(pos, new Vec(2, 0));
+            return new Lava(pos, new Vec(2, 0)); // move to left or right
         } else if (ch == "|") {
-            return new Lava(pos, new Vec(0, 2));
+            return new Lava(pos, new Vec(0, 2)); // move down
         } else if (ch == "v") {
-            return new Lava(pos, new Vec(0, 3), pos);
+            return new Lava(pos, new Vec(0, 3), pos); // move down and back to its position before
         }
     }
 }
 Lava.prototype.size = new Vec(1, 1);
+// console.log(Lava.prototype.size);
 
-// coins would move up and down synchronously, the starting phase of each coin is randomized t avoid that.
+
+// coins would move up and down synchronously, the starting phase of each coin is randomized to avoid that.
 // multiply the value returned by Math.random by 2 π to give the coin a random starting position on the wave
 // why 2 π ? the width of a wave a circle produces, is 2 π
+// ? Umfang eines Kreises = 2π * r, random gibt Zahl zwischen 0 und 1 aus, also eine Position auf der Kreisbahn
+// ? Anfang = 0, Ende = 1, 0 und 1 gleiche Position 
 
 class Coin {
     constructor(pos, basePos, wobble) {
@@ -142,13 +157,15 @@ class Coin {
         return "coin";
     }
     static create(pos) {
-        let basePos = pos.plus(new Vec(0.2, 0.1));
+        let basePos = pos.plus(new Vec(0.2, 0.1)); // shift the starting position to center the coins
         return new Coin(basePos, basePos, Math.random() * Math.PI * 2);
     }
 }
 Coin.prototype.size = new Vec(0.6, 0.6);
 
 // define the levelChars object that maps the plan characters to either background grid types or actor classes.
+// ? has to be after creating the classes
+
 const levelChars = {
     ".": "empty",
     "#": "wall",
@@ -160,18 +177,14 @@ const levelChars = {
     "v": Lava
 };
 
-/// ----------- First Level Creation / Connection ------ ///
-
-// --> html/window.onload
-// let simpleLevel = new Level(simpleLevelPlan);
-// console.log(simpleLevel);
-
 
 /// ----------- Function for Base Element Creation With Attributes -------- ///
 
 // this function provides a short and precise way to create an element and give it some attributes
 // and child nodes. It is going to be reused in the following functions afterwards every time new
 // elements and attributes are needed.
+// ? name => which html-element(e.g. Div, table...), attrs => classes or style-attributes for this element,
+// ...children => div(game) => table(background) => tr(row) => td(block)
 
 function elt(name, attrs, ...children) {
     let dom = document.createElement(name);
@@ -181,6 +194,7 @@ function elt(name, attrs, ...children) {
     for (let child of children) {
         dom.appendChild(child);
     }
+    // console.log(dom);
     return dom;
 }
 
@@ -192,11 +206,9 @@ function elt(name, attrs, ...children) {
 // itself and a level object.
 class DOMDisplay {
     constructor(parent, level) {
-        this.dom = elt("div", {
-            class: "game"
-        }, drawGrid(level));
+        this.dom = elt("div", {class: "game"}, drawGrid(level));
         this.actorLayer = null; // follow actor element
-        parent.appendChild(this.dom);
+        parent.appendChild(this.dom); // gameDiv append to html body        
     }
     clear() {
         this.dom.remove();
@@ -210,43 +222,24 @@ class DOMDisplay {
 const scale = 20;
 
 function drawGrid(level) {
-    return elt("table", {
-        class: "background",
-        // style: "width: " + level.width * scale + "px"
-        style: `width: ${level.width * scale}px`
-    }, ...level.rows.map(row =>
-        elt("tr", {
-                style: `height: ${scale}px`
-            },
-            // elt("tr", {style: "height: " + scale + "px"},
-            ...row.map(type => elt("td", {
-                class: type
-            })))
-    ));
+    // console.log(level);
+    return elt("table", {class: "background", style: `width: ${level.width * scale}px`}, 
+    ...level.rows.map(row => elt("tr", {style: `height: ${scale}px`}, 
+    ...row.map(type => elt("td", {class: type})))));
 }
 
 
 /// --------------- Drawing Of Single Actors With Scaling And Positioning --------- ///
 
-// creating DOM-elements
-
 function drawActors(actors) {
+    // console.log(actors);
+    // create a div with actor-divs inside, each gets size- and position-values inside their style-attributes
     return elt("div", {}, ...actors.map(actor => {
-
-        // let rect = elt("div", {class: "actor" + actor.type});
-        // rect.style.width = actor.size.x * scale + "px";
-        // rect.style.height = actor.size.y * scale + "px";
-        // rect.style.left = actor.pos.x * scale + "px";
-        // rect.style.top = actor.pos.y * scale + "px";
-
-        let rect = elt("div", {
-            class: `actor ${actor.type}`
-        });
+        let rect = elt("div", {class: `actor ${actor.type}`});
         rect.style.width = `${actor.size.x * scale}px`;
         rect.style.height = `${actor.size.y * scale}px`;
         rect.style.left = `${actor.pos.x * scale}px`;
         rect.style.top = `${actor.pos.y * scale}px`;
-
         return rect;
     }));
 }
@@ -256,20 +249,19 @@ function drawActors(actors) {
 // use syncState to do so.
 // The actorLayer property will be used to track the element that holds the actors so that they
 // can be easily removed and replaced
-DOMDisplay.prototype.syncState = function (state) {
+DOMDisplay.prototype.syncState = function(state) {
     if (this.actorLayer) {
         this.actorLayer.remove();
     }
     this.actorLayer = drawActors(state.actors);
     this.dom.appendChild(this.actorLayer);
-    // this.dom.className = "game" + state.status;
     this.dom.className = `game ${state.status}`;
     this.scrollPlayerIntoView(state);
 };
 
 /// ------ Detect Position of Actor to Set Position of Game Screen ------ ///
 
-DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
+DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
     let width = this.dom.clientWidth;
     let height = this.dom.clientHeight;
     let margin = width / 3;
@@ -296,11 +288,11 @@ DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
 };
 
 
-///* ------------------ Movement ------------------///
+/// ------------------ Movement ------------------ ///
 
 
-Level.prototype.touches = function (pos, size, type) {
-    var xStart = Math.floor(pos.x); //floor rounds down
+Level.prototype.touches = function(pos, size, type) {
+    var xStart = Math.floor(pos.x); // floor rounds down
     var xEnd = Math.ceil(pos.x + size.x); // ceil rounds up
     var yStart = Math.floor(pos.y);
     var yEnd = Math.ceil(pos.y + size.y);
@@ -319,7 +311,7 @@ Level.prototype.touches = function (pos, size, type) {
     return false
 }
 
-State.prototype.update = function (time, keys) {
+State.prototype.update = function(time, keys) {
     let actors = this.actors.map(actor => actor.update(time, this, keys));
     let newState = new State(this.level, actors, this.status);
 
@@ -348,11 +340,11 @@ function overlap(actor1, actor2) {
         actor1.pos.y < actor2.pos.y + actor2.size.y;
 }
 
-Lava.prototype.collide = function (state) {
+Lava.prototype.collide = function(state) {
     return new State(state.level, state.actors, "lost");
 }
 
-Coin.prototype.collide = function (state) {
+Coin.prototype.collide = function(state) {
     let filtered = state.actors.filter(a => a != this);
     let status = state.status;
 
@@ -362,7 +354,7 @@ Coin.prototype.collide = function (state) {
     return new State(state.level, filtered, status);
 }
 
-Lava.prototype.update = function (time, state) {
+Lava.prototype.update = function(time, state) {
     let newPos = this.pos.plus(this.speed.times(time));
 
     if (!state.level.touches(newPos, this.size, "wall")) {
@@ -431,7 +423,6 @@ function trackKeys(keys) {
 
     return down;
 }
-
 const arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"])
 
 
@@ -440,8 +431,7 @@ function runAnimation(frameFunc) {
 
     function frame(time) {
         if (lastTime != null) {
-            let timeStep = Math.min(time - lastTime, 100) / 1000;  //! warum 100 ?
-
+            let timeStep = Math.min(time - lastTime, 100) / 1000;  // 100ms / 1000 => seconds
             if (frameFunc(timeStep) === false) {
                 return;
             }
